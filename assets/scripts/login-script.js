231e281cc7727e489022403571ec27cec2442a9e
ZulfadarIA <90242916+ZulfadarIA@users.noreply.github.com>
2022-08-10 20:58:57 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -15,10 +15,25 @@ const expectedPassword = 'superpassword';
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /* 
+   * Membuat variabel email untuk menyimpan email password yang didapatkan saat button ditekan.
+   * @Constant {string}
+  */
   const email = inputEmailElement.value;
+  
+  /* 
+   * Membuat variabel password untuk menyimpan nilai password yang didapatkan saat button ditekan.
+   * @Constant {string}
+  */
   const password = inputPasswordElement.value;
+  
+  /* Comment : Memastikan bahwa niali email dan password sesuai denagn nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
+    
+    /* Comment : jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
+    
+ /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
   } else {
     showPopUp();
   }
